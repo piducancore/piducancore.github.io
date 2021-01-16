7f2ec582200537dfd96fcca064e1aea533f94b92
Rodrigo Moreira <hola@piducan.dev>
2021-01-16 20:14:37 -0300

    add mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "piducan.dev",
+  },
+  plugins: ["gatsby-plugin-mdx"],
 }
