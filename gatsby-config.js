a648051489e958bb008a0d79868d167772163bbe
Rodrigo Moreira <hola@piducan.dev>
2021-01-19 11:12:36 -0300

    add catch-links and remark-images plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,29 @@ module.exports = {
     title: "piducan.dev",
   },
   plugins: [
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-sharp",
+    "gatsby-remark-images",
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
         defaultLayouts: {
           default: require.resolve("./src/components/Layout.js"),
         },
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
       },
     },
     "gatsby-plugin-theme-ui",
